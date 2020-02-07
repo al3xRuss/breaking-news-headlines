8d3a93285ae8172500aa27ce4f33f159c68c0690
Alex Russ <alexRuss@alexs-mbp-10.attlocal.net>
2020-02-07 15:17:33 -0600

    Stagger List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, transition, style, animate, query, stagger} from '@angular/animations';
 import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(-15px)' }),
+            stagger(
+              '50ms',
+              animate(
+                '550ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0px)' })
+              )
+            )
+          ],
+          { optional: true }
+        ),
+        query(':leave', animate('50ms', style({ opacity: 0 })), {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
-  title = 'Assessment';
+  title = 'Breaking News';
   apiData: object;
+  length = 0
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
     this.data.getData().subscribe(data => {
       this.apiData = data;
+      this.length = Object.keys(this.data).length;
       console.log(data);
     });
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
